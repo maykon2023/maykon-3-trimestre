fba4593121f0ac73a0079a780697b6f693c893a0
maykon2023 <161834039+maykon2023@users.noreply.github.com>
2024-10-20 21:51:41 -0300

    20/10

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    '0 que e um carro',
+    'O que é uma corrida',
+    'corrida e uma competicao entre varios carros e quem chegar primeiro ganha'
+)
+
+criaCartao(
+    'veiculos',
+    'Quantos reais custa um carro popular?',
+    '100 mil reais'
+)
+
+criaCartao(
+    'existe quantas categorias de veiculos ',
+    'O que é um onibus?',
+    'onibus e um transporte publico que leva varias pessoas de um ponto para o outro'
+)
